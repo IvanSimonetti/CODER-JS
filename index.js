6f48e05725acc7906c81b0dcbe44b75fb4512a1e
Ivan Simonetti <ivansimonetti81@gmail.com>
2022-06-30 18:16:52 -0300

    Desafio complementario usando For

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("El js anda piola");
+/* Aprendiendo a sumar del 0 al 10*/
+let numero = parseInt(prompt("Escribí un número"));
+
+for(let i = 0; i <= 9; i++){
+        let resultado = numero + i;
+        let mensaje = `${numero} + ${i} = ${resultado}`;
+        alert(mensaje);      
+}
+
+        
+       
+

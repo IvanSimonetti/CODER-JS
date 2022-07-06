ec40cfeab3c4f6acd20e450baac508c6763921f7
IvanSimonetti <100242991+IvanSimonetti@users.noreply.github.com>
2022-07-05 21:04:50 -0300

    Desafio Simulador Interactivo

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-/* Aprendiendo a sumar del 0 al 10*/
-let numero = parseInt(prompt("Escribí un número"));
+/*Simulador pago en cuotas*/
 
-for(let i = 0; i <= 9; i++){
-        let resultado = numero + i;
-        let mensaje = `${numero} + ${i} = ${resultado}`;
-        alert(mensaje);      
+/*Calcular cuotas sobre un producto determinado*/
+
+let nombreProducto = (prompt("Ingrese el nombre del producto"));
+
+let precio = parseInt(prompt("Ingrese el precio final del producto"));
+
+let cuotas = parseInt(prompt("Ingrese las cuotas a pagar por mes"));
+
+if((cuotas <= 12) && (cuotas > 0)){
+    let mensaje = alert(`las cuotas ingresadas son ${cuotas}`);
+    let operacion = parseInt(precio / cuotas);
+    resultado = alert(`El precio de las cuotas del ${nombreProducto} es de $ ${operacion}`);
+    let compra = prompt("Deseas comprar el producto");
+    switch(compra){
+        case "si":
+            alert("El articulo fue comprado con exito");
+            alert(`Pagaras ${cuotas} cuotas de $ ${operacion}`);
+            break;
+        case "no":
+            alert("No has comprado el producto, vuelve a intentar");
+            break;   
+        default:
+            alert("No ingresaste una opcion valida");    
+    }
+}else{
+    alert("No ingresaste la cantidad de cuotas");
+    alert("El articulo no pudo ser vendido");
 }
 
-        
-       
+
+
 

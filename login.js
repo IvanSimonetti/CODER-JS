a45b05e4ff790524674ad628721894ccd12e90ba
IvanSimonetti <100242991+IvanSimonetti@users.noreply.github.com>
2022-08-04 22:48:56 -0300

    Desafio Incorporando Librerias

--- login.js
+++ login.js
@@ -16,11 +16,32 @@ function form(e){
     if(e.target.children[0].value.includes((("@") || ("+") || ("-") || ("*") || ("/")))) {
         mensaje.innerHTML = "<div>No es un usuario valido</div>";
         mensaje.className = "rojo";
+            Swal.fire({
+                icon: 'error',
+                title: 'Error!',
+                text: 'No ingresaste un usuario',
+                footer: '<a href="index.html">Volver a ingresar</a>'
+            })
     }else{
         const texto = `Usuario: ${e.target.children[0].value} , Clave: ${e.target.children[1].value}`;
-        mensaje.innerHTML = "<h1>Bienvenido a Quizatech!</h1>";
-        mensaje.className = "azul";
-        alert(texto);
+        mensaje.innerHTML = `<h1>Bienvenido a Quizatech, ${texto}</h1>`;
+        mensaje.className = `azul`;
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'Ingresaste correctamente'
+          })
         sessionStorage.setItem("usuario", JSON.stringify ({"nombre":e.target.children[0].value,"clave":e.target.children[1].value}));
     }    
 }
--- productos.js
+++ productos.js
@@ -4,6 +4,11 @@ const productos = [
     { id: 1, nombre:"Procesador",precio: 3500, vendido:false},
     { id: 2, nombre:"Mouse",precio: 3000,vendido:false},
     { id: 3, nombre:"Motherboard",precio: 11000,vendido:false},
+    { id: 4, nombre:"Teclado",precio: 110,vendido:false},
+    { id: 5, nombre:"Memoria Ram",precio: 4200,vendido:false},
+    { id: 6, nombre:"Disco",precio: 7200,vendido:false},
+    { id: 7, nombre:"Mousepad",precio:2800,vendido:false},
+    { id: 7, nombre:"Pc Armada",precio:70000,vendido:false},
 ];
 
 //Guardamos en el localStorage los productos.
@@ -23,32 +28,125 @@ botonTres.addEventListener("click",compraArticulotres);
 //Funciones Compra articulos
 
 function compraArticulouno(){
-    if(botonUno){
-        alert("Compraste el Procesador correctamente");
-        botonUno.innerHTML = "<div><p>El procesador fue vendido con exito</p></div>";
-        botonUno.className = "azul";
-    }else{
-        alert("No pudimos realizar la compra");
-    }
+  if(botonUno){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+      buttonsStyling: false
+    })
+          
+    swalWithBootstrapButtons.fire({
+      	title: 'Atencion',
+        text: "Confirmas la compra?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, seguro',
+        cancelButtonText: 'No, me arrepiento',
+        reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Comprado!',
+          'Tu compra a sido aprobada!',
+          'success'
+        )
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {swalWithBootstrapButtons.fire(
+          'Borrado!',
+          'Tu compra a sido cancelada',
+          'error'
+      )}
+    })
+
+    botonUno.innerHTML = `<div><p>Vendido</p></div>`;
+    botonUno.className = `azul`;
+  }else{
+    alert("No pudimos realizar la compra");
+  }
 }
 
 function compraArticulodos(){
-    if(botonDos){
-        alert("Compraste el Mouse correctamente");
-        botonDos.innerHTML = "<div><p>El mouse fue vendido con exito</p></div>";
-        botonDos.className = "azul";
-    }else{
-        alert("No pudimos realizar la compra");
-    }
+  if(botonDos){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+      buttonsStyling: false
+    })
+          
+    swalWithBootstrapButtons.fire({
+      	title: 'Atencion',
+        text: "Confirmas la compra?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, seguro',
+        cancelButtonText: 'No, me arrepiento',
+        reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Comprado!',
+          'Tu compra a sido aprobada!',
+          'success'
+        )
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {swalWithBootstrapButtons.fire(
+          'Borrado!',
+          'Tu compra a sido cancelada',
+          'error'
+      )}
+    })
+
+    botonDos.innerHTML = `<div><p>Vendido</p></div>`;
+    botonDos.className = `azul`;
+  }else{
+    alert("No pudimos realizar la compra");
+  }
 }
 
 function compraArticulotres (){
-    if(botonTres){
-        alert("Compraste el Motherboard correctamente");
-        botonTres.innerHTML = "<div><p>El motherboard fue vendido con exito</p></div>";
-        botonTres.className = "azul";
-    }else{
-        alert("No pudimos realizar la compra");
-    }
+  if(botonTres){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+      buttonsStyling: false
+    })
+          
+    swalWithBootstrapButtons.fire({
+      	title: 'Atencion',
+        text: "Confirmas la compra?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, seguro',
+        cancelButtonText: 'No, me arrepiento',
+        reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Comprado!',
+          'Tu compra a sido aprobada!',
+          'success'
+        )
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {swalWithBootstrapButtons.fire(
+          'Borrado!',
+          'Tu compra a sido cancelada',
+          'error'
+      )}
+    })
+
+    botonTres.innerHTML = `<div><p>Vendido</p></div>`;
+    botonTres.className = `azul` ;
+  }else{
+    alert("No pudimos realizar la compra");
+  }
 }
 
